cbdb9c6863e1e2fd13a4631557a8f6236674960f
Saurabh Pandey <sp8355063538@gmail.com>
2026-02-01 21:59:26 +0530

    Add basic Java programs and workspace config
    Added several introductory Java programs: arithmetic operations, hello world, variable usage, user input, and even/odd check. Also included a VS Code workspace configuration file.

---
+++ user_input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class user_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a;
+        a = sc.nextLine();
+        System.err.println(a);
+        sc.close();
+    }
+}
---
+++ veriable.java
@@ -0,0 +1,10 @@
+public class veriable {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        // int a,b,c=a+b;
+        System.out.println("answer => "+ c);
+        System.out.println(a+b);
+    }
+}
